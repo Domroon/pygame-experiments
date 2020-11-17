45931a81d1973dd4ddbe4f3d8098bf1acf1724da
Dominik Haeusser <haeusser-dominik@web.de>
2020-11-17 12:28:49 +0100

    try from video 9

--- techWithTim.py
+++ techWithTim.py
@@ -15,6 +15,9 @@ char = pygame.image.load('standing.png')
 
 clock = pygame.time.Clock()
 
+music = pygame.mixer.music.load("music.mp3")
+pygame.mixer.music.play(-1)
+
 score = 0
 
 class Player:
@@ -50,6 +53,22 @@ class Player:
                 win.blit(walkLeft[0], (self.x, self.y))
         self.hitbox = (self.x + 17, self.y + 11, 29, 52)
         # pygame.draw.rect(win, (255, 0, 0), self.hitbox, 2)
+
+    def hit(self):
+        self.x = 60
+        self.y = 410
+        self.walkCount = 0
+        font1 = pygame.font.SysFont('comicsans', 100)
+        text = font1.render('-5', 1, (255, 0, 0))
+        win.blit(text, (250 - (text.get_width()/2), 200))
+        pygame.display.update()
+        i = 0
+        while i < 100:
+            pygame.time.delay(10)
+            i += 1
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
 
 
 class Projectile:
@@ -148,6 +167,11 @@ bullets = []
 run = True
 while run:
     clock.tick(27)  # fps = 27
+
+    if man.hitbox[1] < goblin.hitbox[1] + goblin.hitbox[3] and man.hitbox[1] + man.hitbox[3] > goblin.hitbox[1]:
+        if man.hitbox[0] + man.hitbox[2] > goblin.hitbox[0] and man.hitbox[0] < goblin.hitbox[0] + goblin.hitbox[2]:
+            man.hit()
+            score -= 5
 
     if shootLoop > 0:
         shootLoop += 1
