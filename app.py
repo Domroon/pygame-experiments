4e778a38f9005e679028105e33370bb6ea898f31
Dominik Haeusser <haeusser-dominik@web.de>
2020-11-17 12:07:16 +0100

    add new ball

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ black = 0, 0, 0
 
 screen = pygame.display.set_mode(size)
 
-ball = pygame.image.load("intro_ball.gif")
+ball = pygame.image.load("new_ball.gif")
 ballrect = ball.get_rect()
 
 while 1:
