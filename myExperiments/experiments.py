849af589ef99787d2739a9ec8c9169d3ea10e800
Dominik Haeusser <haeusser-dominik@web.de>
2020-11-18 12:48:55 +0100

    collision with borders works correct

--- myExperiments/experiments.py
+++ myExperiments/experiments.py
@@ -3,8 +3,8 @@ import random
 
 pygame.init()
 
-screenWidth = 250
-screenHeight = 250
+screenWidth = 700
+screenHeight = 500
 
 window = pygame.display.set_mode((screenWidth, screenHeight))
 
@@ -14,13 +14,14 @@ clock = pygame.time.Clock()
 
 
 class SnakeStartRect:
-    def __init__(self, x, y, width, height, color, velocity):
+    def __init__(self, x, y, width, height, color, x_velocity, y_velocity):
         self.x = x
         self.y = y
         self.width = width
         self.height = height
         self.color = color
-        self.velocity = velocity
+        self.x_velocity = x_velocity
+        self.y_velocity = y_velocity
 
     def draw(self, window):
         pygame.draw.rect(window, self.color, (self.x, self.y, self.width, self.height))
@@ -34,15 +35,16 @@ def redraw(startRect):
 
 def main():
     # define middle of the rectangle
-    rectWidth = 20
-    rectHeight = 20
+    rectWidth = 10
+    rectHeight = 10
     middleXofRect = (screenWidth-rectWidth)/2
     middleYofRect = (screenHeight-rectHeight)/2
 
     # initialize one Rectangle - Object
-    randomStartXvelocity = random.randint(-2, 2)
-    randomStartXvelocity = random.randint(-2, 2)
-    startRect = SnakeStartRect(middleXofRect, middleYofRect, rectWidth, rectHeight, (255, 0, 0), 1)
+    randomStartXvelocity = random.randint(-1, 1)
+    randomStartYvelocity = random.randint(-1, 1)
+    startRect = SnakeStartRect(middleXofRect, middleYofRect+70,
+                               rectWidth, rectHeight, (255, 0, 0), randomStartXvelocity, randomStartYvelocity)
 
     # Screen Borders
     rightScreenBorder = screenWidth - startRect.width
@@ -50,21 +52,29 @@ def main():
     topScreenBorder = 0
     bottomScreenBorder = screenHeight - startRect.height
 
+    #x - Directions
+    x_directions = [-1, 0, 1]
+    y_directions = [-1, 0, 1]
+
     run = True
     while run:
-        clock.tick(100)  # fps = 27
+        clock.tick(500)  # fps = 27
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
 
-        startRect.x += startRect.velocity
-        # startRect.y += startRect.velocity
+        startRect.x += startRect.x_velocity
+        startRect.y += startRect.y_velocity
 
-        if startRect.x > rightScreenBorder or startRect.x == leftScreenBorder:
-            startRect.velocity *= -1
-        elif startRect.y == topScreenBorder or startRect.y > bottomScreenBorder:
-            startRect.velocity *= -1
+        if startRect.y == topScreenBorder:
+            startRect.y_velocity *= -1
+        if startRect.x == rightScreenBorder:
+            startRect.x_velocity *= -1
+        if startRect.y == bottomScreenBorder:
+            startRect.y_velocity *= -1
+        if startRect.x == leftScreenBorder:
+            startRect.x_velocity *= -1
 
         redraw(startRect)
 
